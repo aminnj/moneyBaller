0f5416a6e1ee7165f2f4d40c77106733408c084b
ocolegro <ocolegro@u.rochester.edu>
2016-02-06 19:59:20 -0800

    Update scrape_fantcrunch.py

--- scrape/scrape_fantcrunch.py
+++ scrape/scrape_fantcrunch.py
@@ -5,25 +5,30 @@ import numpy as np
 import Games
 
 seasons = [2014,2015]
+sources = ['fantasyfeud','fanduel','draftkings','fantasyaces','yahoo']
+###Modified to pick up all sources on fantcrunch.  This makes the other scraper pretty obsolete.
 g = Games.Games(years=seasons, debug=False)
-for year in seasons:
-    datesToFetch = np.unique(np.array(map(g.get_date_from_gameid, g.get_game_ids(years=[year])))) # integers
-    d = {}
-    for date in datesToFetch:
-        datestr = "%s-%s-%s" % (str(date)[:4], str(date)[4:6], str(date)[6:]) # convert to 2013-01-09
-        try:
-            data = requests.get("https://www.fantasycruncher.com/lineup-rewind/draftkings/NBA/%s" % datestr)
-            bs = bs4.BeautifulSoup(data.text,"html.parser")
-            rawjson = None
-            for script in bs.findAll("script", {"src":""}):
-                if "var playerlist = {" in script.text:
-                    rawjson = script.text
-                    break
-            rawjson = "{"+rawjson.split("\n")[1].split("{",1)[1][:-2].replace("\\","")
-            js = json.loads(rawjson)
-            d[date] = copy.deepcopy(js)
-            print "fetched %s" % datestr
-        except: print "ERROR fetching %s" % datestr
+for source in sources:
+    for year in seasons:
+        print 'getting dates'
+        print 'the game ids  are ' + str(g.get_game_ids(years=[year]))
+        datesToFetch = np.unique(np.array(map(g.get_date_from_gameid, g.get_game_ids(years=[year])))) # integers
+        print datesToFetch
+        d = {}
+        for date in datesToFetch:
+            datestr = "%s-%s-%s" % (str(date)[:4], str(date)[4:6], str(date)[6:]) # convert to 2013-01-09
+            try:
+                data = requests.get("https://www.fantasycruncher.com/lineup-rewind/%s/NBA/%s" % (source,datestr))
+                bs = bs4.BeautifulSoup(data.text,"html.parser")
+                rawjson = None
+                for script in bs.findAll("script", {"src":""}):
+                    if "var playerlist = {" in script.text:
+                        rawjson = script.text
+                        break
+                rawjson = "{"+rawjson.split("\n")[1].split("{",1)[1][:-2].replace("\\","")
+                js = json.loads(rawjson)
+                d[date] = copy.deepcopy(js)
+                print "fetched %s" % datestr
+            except: print "ERROR fetching %s" % datestr
 
-    with gzip.open("../data/pickle/fantasycruncher_%i.pkl" % year, "wb") as fh: pickle.dump(d, fh)
-
+        with gzip.open("../data/pickle/%s_%i.pkl" % (sources,year), "wb") as fh: pickle.dump(d, fh)
