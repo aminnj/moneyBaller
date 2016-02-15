63ebf70e9d6ec4f9a86cfc8ecfc56002dcc11f19
ocolegro <ocolegro@u.rochester.edu>
2016-02-15 14:42:21 -0800

    Update Load_Games.py

--- scrape/Load_Games.py
+++ scrape/Load_Games.py
@@ -22,7 +22,14 @@ class Load_Games:
         if type == "P":
             avg_strings = ['AVG_'  + s for s in np.array(self.p_fint).astype(str)]
         if type == "T":
-            avg_strings = ['AVG_'  + s for s in np.array(self.t_fint).astype(str)]
+            avg_strings = []
+            for line in self.positions:
+                temp = ['AVG_'  + s +'_' +line for s in np.array(self.t_fint).astype(str)]
+                if len(avg_strings) == 0:
+                    avg_strings = temp
+                else:
+                    avg_strings = np.append(avg_strings,temp)
+
         for line in range(len(headers)):
             if line < (len(headers)-1):
                 return_dict[str(headers[line]+suffix)] = line
@@ -33,24 +40,26 @@ class Load_Games:
 
         for line in range(len(avg_strings)):
             return_dict[avg_strings[line]+suffix] = line + len(headers)
+
         if type == "P":
+            return_dict["POSITION"]              = len(headers)+len(avg_strings) + 2
             return_dict["FANT_TARGET"]       = len(headers)+len(avg_strings)
             return_dict["REST"]              = len(headers)+len(avg_strings) + 1
-            return_dict["FANDUEL_SALARY"]    = len(headers)+len(avg_strings) +2
-            return_dict["DRAFTKINGS_SALARY"] = len(headers)+len(avg_strings) + 3
-            return_dict["FANT_PREDICTION"]   = len(headers)+len(avg_strings) + 4
+            return_dict["FANDUEL_SALARY"]    = len(headers)+len(avg_strings) + 3
+            return_dict["DRAFTKINGS_SALARY"] = len(headers)+len(avg_strings) + 4
+            return_dict["FANT_PREDICTION"]   = len(headers)+len(avg_strings) + 5
 
         return return_dict
 
     def set_fant(self,dict_,stats_):
         stats_[:,dict_['FANT']] = 3.0 * np.nan_to_num(stats_[:,dict_['FG3M']].astype(float))\
                                   + 2.0 *   np.nan_to_num(stats_[:,dict_['FGM']].astype(float))\
-                                    + 1.0 *  np.nan_to_num(stats_[:,dict_['FTM']].astype(float))\
-                                      + 1.2 *  np.nan_to_num(stats_[:,dict_['REB']].astype(float))\
-                                        + 1.5 *  np.nan_to_num(stats_[:,dict_['AST']].astype(float))\
-                                          + 2.0 *  np.nan_to_num(stats_[:,dict_['BLK']].astype(float))\
-                                            + 2.0 *  np.nan_to_num(stats_[:,dict_['STL']].astype(float))\
-                                              - 1.0 *   np.nan_to_num(stats_[:,dict_['TOV']].astype(float))
+                                  + 1.0 *  np.nan_to_num(stats_[:,dict_['FTM']].astype(float))\
+                                  + 1.2 *  np.nan_to_num(stats_[:,dict_['REB']].astype(float))\
+                                  + 1.5 *  np.nan_to_num(stats_[:,dict_['AST']].astype(float))\
+                                  + 2.0 *  np.nan_to_num(stats_[:,dict_['BLK']].astype(float))\
+                                  + 2.0 *  np.nan_to_num(stats_[:,dict_['STL']].astype(float))\
+                                  - 1.0 *   np.nan_to_num(stats_[:,dict_['TOV']].astype(float))
 
     def load_season_json(self, year, ptg):
         season = u.yearToSeason(year)
@@ -65,98 +74,211 @@ class Load_Games:
     def load_teams(self):
         for iyear,year in enumerate(self.years):
             t_year = []
+
+            #Load up team data
             t_colnames, t_data = self.load_season_json(year, "T") # team data for matches
-            t_data_init        = np.array(t_data)
+            t_data = np.array(t_data)
+            
+            #Load up player data
+            p_colnames, p_data = self.load_season_json(year, "P") # team data for matches
+            p_data = np.array(p_data)
+            
+            #Open player stats file
+            with gzip.open("../data/pickle/player_stats_%i.pkl" % year,"rb") as fh:
+                data_raw = pickle.load(fh)
+
 
             if iyear ==0:
-                t_dict_init    = self.gen_dict(t_colnames,"None")
-                t_sel_upp,t_opp_low,t_opp_high             = len(t_dict_init)+len(self.t_fint),t_dict_init["FGM"],len(t_dict_init)+len(self.t_fint)
+                #Create initial dictionaries
+                t_d    = self.gen_dict(t_colnames,"None")
+                p_d    = self.gen_dict(p_colnames,"None")
 
+                t_sel_upp,t_opp_low,t_opp_high             = len(t_d)+len(self.t_fint),t_d["FGM"],len(t_d)+len(self.t_fint)
+
+                #Create final dictionaries for individual teams
                 st_dict = self.gen_dict(t_colnames ,"T","_SEL")
                 ot_dict = self.gen_dict(np.array(t_colnames)[range(t_opp_low,len(t_colnames))],"T","_OPP")
 
-                self.t_dict                            = self.gen_dict(np.append([s[0] for s in sorted(st_dict.items(), key=operator.itemgetter(1))],
-                                                                       [s[0] for s in sorted(ot_dict.items(), key=operator.itemgetter(1))])
-                                                                       ,"None","",False)
-            self.set_fant(t_dict_init,t_data_init)                                                   #maps final column to fantasy points
-            t_data_init[:,t_dict_init["WL"]][t_data_init[:,t_dict_init["WL"]] == "W"]            = 1 #maps [W,L] to [0,1]
-            t_data_init[:,t_dict_init["WL"]][t_data_init[:,t_dict_init["WL"]] == "L"]            = 0
-            t_data_init[:,t_dict_init["MATCHUP"]] = np.array([int("@" not in s) for s in t_data_init[:,t_dict_init["MATCHUP"]]]) #maps [A,H] to [0,1]
+                #Merge these dictionaries for composite team info
+                self.t_d                            = self.gen_dict(np.append([s[0] for s in sorted(st_dict.items(), key=operator.itemgetter(1))],
+                                                                       [s[0] for s in sorted(ot_dict.items(), key=operator.itemgetter(1))]),"None","",False)
+            #Map final columns to fantasy points
+            self.set_fant(p_d,p_data)
+            self.set_fant(t_d,t_data)
 
-            t_data_init[:,t_dict_init["GAME_DATE"]] =  np.array(map(lambda x: (datetime.datetime(*map(int,x.split("-"))) - \
-                                                                     datetime.datetime(year=1970, month=1, day=1)).days, t_data_init[:,t_dict_init["GAME_DATE"]] ))
-            t_data_init = t_data_init[t_data_init[:,t_dict_init["GAME_DATE"]].astype(int).argsort()]
-            for tit_, team_id in enumerate(np.unique(t_data_init[:,t_dict_init['TEAM_ID']])):
-                teams_  =    t_data_init[t_data_init[:,t_dict_init['TEAM_ID']] == team_id]
-                teams_[:,t_dict_init["GAME_DATE"]] = teams_[:,t_dict_init["GAME_DATE"]].astype(int) - np.roll(teams_[:,t_dict_init["GAME_DATE"]],1).astype(int)
-                opps_   =    t_data_init[t_data_init[:,t_dict_init['TEAM_ID']] != team_id]
-                sel_res,opp_res = [],[]
+            #Loads up the player positions
+            temp_arr,temp_lines   = [],[]
+            for id_,line in enumerate(p_data):
+                pid = line[p_d['PLAYER_ID']]
+                if (pid,year) not in data_raw:
+                    print 'failed player %s with pid %s for id_ %s' % (line[p_d["PLAYER_NAME"]],pid,id_)
+                    continue
+                else:
+                    temp_arr.append(np.append(line,str(data_raw[(pid,year)]['position'])))
+                    temp_lines.append(id_)
+            p_data = np.c_[p_data[temp_lines],temp_arr]
+
+
+
+            #maps [W,L] to [0,1]
+            t_data[:,t_d["WL"]][t_data[:,t_d["WL"]] == "W"]            = 1
+            t_data[:,t_d["WL"]][t_data[:,t_d["WL"]] == "L"]            = 0
+
+            #maps [A,H] to [0,1]
+            t_data[:,t_d["MATCHUP"]] = np.array([int("@" not in s) for s in t_data[:,t_d["MATCHUP"]]])
+
+            #maps game date to number of days rest
+            t_data[:,t_d["GAME_DATE"]] =  np.array(map(lambda x: (datetime.datetime(*map(int,x.split("-"))) - \
+                                                                     datetime.datetime(year=1970, month=1, day=1)).days, t_data[:,t_d["GAME_DATE"]] ))
+            #sort to make time ordering
+            t_data = t_data[t_data[:,t_d["GAME_DATE"]].astype(int).argsort()]
+
+            for tit_, team_id in enumerate(np.unique(t_data[:,t_d['TEAM_ABBREVIATION']])):
+                #Subset the teams and players
+                teams_  =    t_data[t_data[:,t_d['TEAM_ABBREVIATION']] == team_id]
+                players =    p_data[p_data[:,p_d['TEAM_ABBREVIATION']] == team_id]
+                team_p  = []
 
                 for git_,sel_team in enumerate(teams_):
-                    gid      =  sel_team[t_dict_init["GAME_ID"]].astype(int)
-                    opp_team = opps_[opps_[:,t_dict_init["GAME_ID"]].astype(int) == gid]
-
-                    if len(sel_res) == 0:
-                        sel_res  = sel_team
-                        opp_res  = opp_team
+                    #Cycle through games and find the scores by this team at each position
+                    gid      =  sel_team[t_d["GAME_ID"]].astype(int)
+                    psub     = players[players[:,p_d["GAME_ID"]].astype(int) == gid]
+                    out_ = []
+                    for line in self.positions:
+                        sum_ = np.sum(psub[psub[:,-1] == line][:,p_d["FANT"]])
+                        if sum_ != False:
+                            out_.append(sum_)
+                        else:
+                            out_.append(np.nan)
+                    sel_team = np.append(sel_team,out_)
+                    if len(team_p) == 0:
+                        team_p  = sel_team
                     else:
-                        sel_res = np.vstack((sel_res,sel_team))
-                        opp_res = np.vstack((opp_res,opp_team))
+                        team_p = np.vstack((team_p,sel_team))
 
-                    if git_ == len(teams_)-1:
-                        for range_ in self.t_fint:
-                            opp_res = np.c_[opp_res,u.ma(opp_res[:,t_dict_init["FANT"]].astype(float),range_)]
-                            sel_res = np.c_[sel_res,u.ma(sel_res[:,t_dict_init["FANT"]].astype(float),range_)]
 
-                stack    = np.c_[sel_res[:,range(t_sel_upp)],opp_res[:,range(t_opp_low,t_opp_high)]]
                 if len(t_year) == 0:
-                    t_year = stack
+                    t_year = team_p
                 else:
-                    t_year = np.vstack((t_year,stack))
+                    t_year = np.vstack((t_year,team_p))
+
             if iyear == 0:
                 t_total = t_year
             else:
                 t_total = np.vstack((t_total,t_year))
-        self.teams =  t_total
+
+        merged = []
+        for team_id in np.unique(t_total[:,t_d['TEAM_ABBREVIATION']]):
+            temp_merge = []
+            teams_  =    t_total[t_total[:,t_d['TEAM_ABBREVIATION']] == team_id]
+            opps_   =    t_total[t_total[:,t_d['TEAM_ABBREVIATION']] != team_id]
+
+            #Merge team statistics with opponent statistics (select columns)
+            for g_id,game_id in enumerate(np.unique(teams_[:,t_d['GAME_ID']])):
+                opp_team    = opps_[opps_[:,t_d['GAME_ID']] == game_id][0]
+                temp_merge.append(np.append(teams_[g_id][0:t_sel_upp+len(self.positions)],opp_team[t_opp_low:t_opp_high+len(self.positions)]))
+            temp_merge = np.array(temp_merge)
+
+            #Insert moving averages into the array
+            for pos in self.positions:
+                for range_ in self.t_fint:
+                    if range_ == 1: continue
+                    index_ = int(self.t_d['AVG_' + str(range_) + '_' + pos + '_SEL'])
+                    base_  = int(self.t_d['AVG_1_' + pos + '_SEL'])
+                    temp_merge = np.insert(temp_merge,index_,
+                                     u.ma(temp_merge[:,base_].astype(float),range_),1)
+
+            #Insert moving averages into the array
+            for pos in self.positions:
+                for range_ in self.t_fint:
+                    if range_ == 1: continue
+                    index_ = self.t_d['AVG_' + str(range_) + '_' + pos + '_OPP']
+                    base_  = int(self.t_d['AVG_1_' + pos + '_OPP'])
+                    temp_merge = np.insert(temp_merge,index_,
+                                     u.ma(temp_merge[:,base_].astype(float),range_),1)
+
+            if len(merged) == 0:
+                merged = temp_merge
+            else:
+                merged = np.vstack((merged,np.array(temp_merge)))
+
+        self.teams =  np.array(merged)
 
     def load_players(self):
         p_total = []
         for iyear,year in enumerate(self.years):
+            #Load up player data
             p_year = []
             p_colnames, p_data = self.load_season_json(year, "P") # team data for matches
-            p_data_init        = np.array(p_data)
+            p_data        = np.array(p_data)
+
+            #Open player stats file
+            with gzip.open("../data/pickle/player_stats_%i.pkl" % year,"rb") as fh:
+                data_raw = pickle.load(fh)
 
             if iyear ==0:
+                #Create initial dictionary
                 p_d    = self.gen_dict(p_colnames,"None")
-                self.p_dict    =  self.gen_dict(p_colnames,"P")
-            self.set_fant(p_d,p_data_init) #maps final column to fantasy points
-            p_data_init[:,p_d["MATCHUP"]] = np.array([int("@" not in s) for s in p_data_init[:,p_d["MATCHUP"]]]) #maps [A,H] to [0,1]
 
-            for tit_, player_id in enumerate(np.unique(p_data_init[:,p_d['PLAYER_ID']])):
-                p_init  = p_data_init[p_data_init[:,p_d['PLAYER_ID']] == player_id]
+                #Create final dictionary
+                self.p_d    =  self.gen_dict(p_colnames,"P")
+
+            #maps final column to fantasy points
+            self.set_fant(p_d,p_data)
+
+            #maps [A,H] to [0,1]
+            p_data[:,p_d["MATCHUP"]] = np.array([int("@" not in s) for s in p_data[:,p_d["MATCHUP"]]])
+
+            #maps date to days since 1970
+            p_data[:,p_d["GAME_DATE"]] =  np.array(map(lambda x: (datetime.datetime(*map(int,x.split("-"))) - \
+                                                                     datetime.datetime(year=1970, month=1, day=1)).days, p_data[:,p_d["GAME_DATE"]] ))
+            temp_arr,temp_lines   = [],[]
+            for id_,line in enumerate(p_data):
+                pid = line[p_d['PLAYER_ID']]
+                if (pid,year) not in data_raw:
+                    print 'failed player %s with pid %s for id_ %s' % (line[p_d["PLAYER_NAME"]],pid,id_)
+                    continue
+                else:
+                    temp_arr.append(np.append(line,str(data_raw[(pid,year)]['position'])))
+                    temp_lines.append(id_)
+            p_data = np.c_[p_data[temp_lines],temp_arr]
+
+
+
+            #sorts by this game date
+            p_data = p_data[p_data[:,p_d["GAME_DATE"]].astype(int).argsort()]
+
+
+            for tit_, player_id in enumerate(np.unique(p_data[:,p_d['PLAYER_ID']])):
+                #Load up a player
+                p_init  = p_data[p_data[:,p_d['PLAYER_ID']] == player_id]
                 p_name  = str.split(self.clean_str(str(p_init[:,p_d['PLAYER_NAME']][0]))," ")
 
+                #Check that the name loaded correctly
                 if len(p_name) < 2:
+                    print 'An error occured while trying to load the player named %s' % (p_name)
                     continue
 
-                p_init[:,p_d["GAME_DATE"]] =  np.array(map(lambda x: (datetime.datetime(*map(int,x.split("-"))) - \
-                                                                     datetime.datetime(year=1970, month=1, day=1)).days, p_init[:,p_d["GAME_DATE"]] ))
 
-                p_init = p_init[p_init[:,p_d["GAME_DATE"]].astype(int).argsort()]
-
+                #Appending the players moving averages
                 for range_ in self.p_fint:
                     p_init = np.c_[p_init,u.ma(p_init[:,p_d["FANT"]].astype(float),range_)]
 
+                #Appending the target
                 p_init = np.c_[p_init,np.append(np.roll(p_init[:,p_d["FANT"]],-1)[-1000000:-1],np.nan) ]
 
+                #Appending the players rest
                 p_init = np.c_[p_init,p_init[:,p_d["GAME_DATE"]].astype(int) - np.roll(p_init[:,p_d["GAME_DATE"]],1).astype(int)]
 
+                #Load up the fantcrunch data
                 key     = str(p_name[1] + ", " + p_name[0])
                 if key in self.p_preds.keys():
                     key_s1 = self.p_preds[key]
                 else:
+                    print 'An error occured while trying to load the player named %s' % (p_name)
                     continue
 
+                #Append the crucial fantcrunch datasets
                 p_final = []
                 for g in np.unique(p_init[:,p_d['GAME_DATE']]):
                     if g in np.array(key_s1.keys()).astype(int):
@@ -192,20 +314,33 @@ class Load_Games:
         print pc_colnames
         self.player_cards = np.array(pc_data)
 
-    def __init__(self, years=[2014], player_fant_int = [3,5,9,15,20],team_fant_int = [3,5,9,15,20], preds_file = '../data/pickle/fantcrunch_merged_nozip.pkl',debug=False):
+    def __init__(self, years=[2014], player_fant_int = [3,5,9,15,20],team_fant_int = [1,5,10,20], preds_file = '../data/pickle/fantcrunch_merged_nozip.pkl',debug=False):
+    #Keep 1 in team_fant_int or face an error ~-_-~
 
+        #Years to perform analysis on
         self.years        = years
+
+        #Intervals for player fantasy points
         self.p_fint       = player_fant_int
+
+        #Intervals for team fantasy pts allowed
         self.t_fint       = team_fant_int
+
+        #Position key
+        self.positions    = ['Forward', '', 'Center', 'Center-Forward', 'Guard', 'Guard-Forward', 'Forward-Center', 'Forward-Guard']
+
+        #Scraped fantcrunch data
         with open(preds_file, 'rb') as handle:
             preds = pickle.load(handle)
-
         self.p_preds      = preds
+
+        #Load up the teams first
         t0 = time.time()
         self.load_teams()
         t1 = time.time()
         print 'The time to load teams was %s' %(t1 - t0)
 
+        #Load up the players second
         t0 = time.time()
         self.load_players()
         t1 = time.time()
